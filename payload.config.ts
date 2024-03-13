4b2c6dbead69985cd07e14c9a3e88f11dcce6aed
Addisu <addisuhaile87@gmail.com>
2024-03-13 11:42:16 +0300

    Create a server and install payload

---
+++ payload.config.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import { buildConfig } from "payload/config";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: "DigitalMarketplace",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
+    },
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+});
