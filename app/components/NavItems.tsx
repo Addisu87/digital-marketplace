f141607a6649003c83f9eb3f4fc72228a24b3ac2
Addisu <addisuhaile87@gmail.com>
2024-03-13 09:33:32 +0300

    Create new cart and outside on click

--- app/components/NavItems.tsx
+++ app/components/NavItems.tsx
@@ -1,16 +1,36 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+
+    // Clean up
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,8 +5,12 @@ import MaxWidthWrapper from "@/app/components/MaxWidthWrapper";
 import NavItems from "@/app/components/NavItems";
 import Image from "next/image";
 import eCommerceLogo from "../../public/E-Shop.jpg";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
+  const user = null;
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
@@ -28,6 +32,51 @@ const Navbar = () => {
 
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Register
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
