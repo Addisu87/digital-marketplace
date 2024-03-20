e80559b59076505e7c7e35bcd6cf3116b9628d7a
Addisu <addisuhaile87@gmail.com>
2024-03-20 15:16:46 +0300

    Add provider and trpc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,44 @@
-import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
+import type { Metadata } from 'next';
+import { Nunito_Sans } from 'next/font/google';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import "./globals.css";
-import Navbar from "./components/Navbar";
+import './globals.css';
+import Navbar from './components/Navbar';
+import Providers from './components/providers/Providers';
 
 const NunitoSans = Nunito_Sans({
-  weight: ["300", "400", "500", "700", "800", "900"],
-  subsets: ["latin"],
-  style: ["normal", "italic"],
-  display: "swap",
+	weight: ['400', '700'],
+	subsets: ['latin'],
+	style: ['normal', 'italic'],
+	display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Digital Marketplace",
-  description: "Digital Marketplace",
+	title: 'Digital Marketplace',
+	description: 'Digital Marketplace',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="h-full">
-      <body
-        className={cn(
-          "relative h-full font-sans antialiased",
-          NunitoSans.className
-        )}
-      >
-        <main className="relative flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow flex-1">{children}</div>
-        </main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en' className='h-full'>
+			<body
+				className={cn(
+					'relative h-full font-sans antialiased',
+					NunitoSans.className,
+				)}
+			>
+				<main className='relative flex flex-col min-h-screen'>
+					<Providers>
+						<Navbar />
+						<div className='flex-grow flex-1'>{children}</div>
+					</Providers>
+				</main>
+			</body>
+		</html>
+	);
 }
