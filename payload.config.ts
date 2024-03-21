e73ffa148481573cfbb0792264f93c167396daa9
Addisu <addisuhaile87@gmail.com>
2024-03-21 17:09:37 +0300

    Create Media, Orders, ProdcutFile and Products collection

--- payload.config.ts
+++ payload.config.ts
@@ -6,6 +6,10 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb';
 import { webpackBundler } from '@payloadcms/bundler-webpack';
 import { slateEditor } from '@payloadcms/richtext-slate';
 import { Users } from '@/collections/Users';
+import { Products } from './collections/Products/Products';
+import { Media } from './collections/Media';
+import { ProductFiles } from './collections/ProductFile';
+import { Orders } from './collections/Orders';
 
 dotenv.config({
 	path: path.resolve(__dirname, './.env'),
@@ -13,13 +17,13 @@ dotenv.config({
 
 export default buildConfig({
 	serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-	collections: [Users],
+	collections: [Users, Products, Media, ProductFiles, Orders],
 	routes: {
 		admin: '/sell',
 	},
 	admin: {
 		user: 'users',
-		bundler: webpackBundler(),
+		bundler: webpackBundler(), 
 		meta: {
 			titleSuffix: '- DigitalMarketplace',
 			favicon: '/favicon.ico',
