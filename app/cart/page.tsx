41d3fa062da78cef2e50d5d31e1f0512e05683be
Addisu <addisuhaile87@gmail.com>
2024-03-22 16:08:08 +0300

    Shopping cart implementation

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
+// Shopping cart
 const Page = () => {
 	const { items, removeItem } = useCart();
 
