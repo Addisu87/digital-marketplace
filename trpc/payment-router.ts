560f7af19bd734d13ad955fb27bee78962021204
Addisu <addisuhaile87@gmail.com>
2024-03-26 15:53:15 +0300

    Update payment router

--- trpc/payment-router.ts
+++ trpc/payment-router.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 import type Stripe from 'stripe';
 
-import { privateProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { getPayloadClient } from '../get-payload';
 import { stripe } from '../lib/stripe';
+import { privateProcedure, router } from './trpc';
 
 export const paymentRouter = router({
 	// for creating a checkout session with Stripe
@@ -18,36 +18,46 @@ export const paymentRouter = router({
 				throw new TRPCError({ code: 'BAD_REQUEST' });
 			}
 
-			try {
-				const payload = await getPayloadClient();
+			const payload = await getPayloadClient();
 
-				const { docs: products } = await payload.find({
-					collection: 'products',
-					where: {
-						id: {
-							in: productIds,
-						},
+			const { docs: products } = await payload.find({
+				collection: 'products',
+				where: {
+					id: {
+						in: productIds,
 					},
-				});
+				},
+			});
 
-				const filteredProducts = products.filter((prod) =>
-					Boolean(prod.priceId),
-				);
+			const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
 
-				const order = await payload.create({
-					collection: 'orders',
-					data: {
-						_isPaid: false,
-						products: filteredProducts.map((prod) => prod.id),
-						user: user.id,
-					},
-				});
+			const order = await payload.create({
+				collection: 'orders',
+				data: {
+					_isPaid: false,
+					products: filteredProducts.map((prod) => prod.id),
+					user: user.id,
+				},
+			});
 
-				const line_items = filteredProducts.map((product) => ({
+			const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+			filteredProducts.forEach((product) => {
+				line_items.push({
 					price: product.priceId!,
 					quantity: 1,
-				}));
+				});
+			});
 
+			line_items.push({
+				price: 'price_1OyE07BnQL13PbUdzlOWJ0Ln',
+				quantity: 1,
+				adjustable_quantity: {
+					enabled: false,
+				},
+			});
+
+			try {
 				const stripeSession = await stripe.checkout.sessions.create({
 					success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
 					cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
@@ -62,7 +72,6 @@ export const paymentRouter = router({
 
 				return { url: stripeSession.url };
 			} catch (err) {
-				console.error('Error creating checkout session:', err);
 				return { url: null };
 			}
 		}),
