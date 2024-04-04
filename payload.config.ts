69c886b09f4ac8f3e784b38f59801307645b3791
Addisu <addisuhaile87@gmail.com>
2024-04-04 14:49:06 +0300

    Update payload.config.ts

--- payload.config.ts
+++ payload.config.ts
@@ -18,7 +18,13 @@ dotenv.config({
 
 export default buildConfig({
 	serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-	collections: [Users, Products, Media, ProductFiles, Orders],
+	collections: [
+		Users,
+		Products,
+		Media,
+		ProductFiles,
+		Orders,
+	],
 	routes: {
 		admin: '/sell',
 	},
