96348387dd1b8890bd13f0baa98be507311338cf
Addisu <addisuhaile87@gmail.com>
2024-04-04 14:35:47 +0300

    Update server

--- server.ts
+++ server.ts
@@ -1,17 +1,17 @@
 import express from 'express';
-import { parse } from 'url';
-import path from 'path';
-import bodyParser from 'body-parser';
 import { IncomingMessage } from 'http';
-import nextBuild from 'next/dist/build';
+import { inferAsyncReturnType } from '@trpc/server';
+import { parse } from 'url';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { PayloadRequest } from 'payload/types';
 
 import { getPayloadClient } from './get-payload';
 import { nextApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
-import { inferAsyncReturnType } from '@trpc/server';
 import { stripeWebhookHandler } from './webhooks';
-import { PayloadRequest } from 'payload/types';
+import nextBuild from 'next/dist/build';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -24,7 +24,9 @@ const createContext = ({
 	res,
 });
 
-export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+export type ExpressContext = inferAsyncReturnType<
+	typeof createContext
+>;
 
 export type WebhookRequest = IncomingMessage & {
 	rawBody: Buffer;
@@ -37,7 +39,11 @@ const start = async () => {
 		},
 	});
 
-	app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler);
+	app.post(
+		'/api/webhooks/stripe',
+		webhookMiddleware,
+		stripeWebhookHandler,
+	);
 
 	const payload = await getPayloadClient({
 		initOptions: {
@@ -50,7 +56,9 @@ const start = async () => {
 
 	if (process.env.NEXT_BUILD) {
 		app.listen(PORT, async () => {
-			payload.logger.info('Next.js is building for production');
+			payload.logger.info(
+				'Next.js is building for production',
+			);
 
 			// @ts-expect-error
 			await nextBuild(path.join(__dirname, '../'));
@@ -61,7 +69,6 @@ const start = async () => {
 		return;
 	}
 
-	// Add security
 	const cartRouter = express.Router();
 
 	cartRouter.use(payload.authenticate);
@@ -69,7 +76,8 @@ const start = async () => {
 	cartRouter.get('/', (req, res) => {
 		const request = req as PayloadRequest;
 
-		if (!request.user) return res.redirect('/sign-in?origin=cart');
+		if (!request.user)
+			return res.redirect('/sign-in?origin=cart');
 
 		const parsedUrl = parse(req.url, true);
 		const { query } = parsedUrl;
