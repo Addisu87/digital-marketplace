2f3e5c3d99c236847a1fbca49189cac13d3d7a1e
Addisu <addisuhaile87@gmail.com>
2024-05-15 11:27:50 +0300

    Update next.js

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,10 @@ const nextConfig = {
 		formats: ['image/avif', 'image/webp'],
 		remotePatterns: [
 			{
-				hostname: '127.0.0.1',
 				protocol: 'http',
+				hostname: 'localhost',
+				port: '*',
+				pathname: '/media/**',
 			},
 		],
 	},
